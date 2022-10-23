9b800ff31b09f8e79acd5ce9a29b7e55a5f4652b
Cesar <cesar_a_mar_@hotmail.com>
2022-10-23 16:37:41 -0500

    Adicion del metodo crear vacante

--- src/main/java/net/cmartinez/controller/CategoriasController.java
+++ src/main/java/net/cmartinez/controller/CategoriasController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(value="/categorias")
@@ -21,8 +22,10 @@ public class CategoriasController
 		return "categorias/formCategoria";
 	}
 	@PostMapping("/save")
-	public String guardar()
+	public String guardar(@RequestParam("nombre") String nombre, @RequestParam("descripcion") String descripcion)
 	{
+		System.out.println("Categoria: " + nombre);
+		System.out.println("Descripcion: " + descripcion);
 		return "categorias/listCategorias";
 	}
 }
