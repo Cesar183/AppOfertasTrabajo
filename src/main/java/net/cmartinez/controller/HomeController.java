1bc521f163d100d6efcfb619010778a01953203a
Cesar <cesar_a_mar_@hotmail.com>
2022-10-23 17:09:11 -0500

    Creación de la clase servcio

--- src/main/java/net/cmartinez/controller/HomeController.java
+++ src/main/java/net/cmartinez/controller/HomeController.java
@@ -1,24 +1,25 @@
 package net.cmartinez.controller;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import net.cmartinez.model.Vacante;
+import net.cmartinez.service.IVacantesService;
 
 @Controller
 public class HomeController 
 {
+	@Autowired
+	private IVacantesService serviceVacantes;
 	@GetMapping("/tabla")
 	public String mostrarTabla(Model model) 
 	{
-		List<Vacante> lista = getVacantes();
+		List<Vacante> lista = serviceVacantes.buscarTodas();
 		model.addAttribute("vacantes", lista);
 		return "tabla";
 	}
@@ -61,67 +62,5 @@ public class HomeController
 		model.addAttribute("salario", salario);
 		model.addAttribute("vigente", vigente);
 		return "home";
-	}
-	/**
-	 * Metodo que regresa una lista de objetos de tipo vacante
-	 */
-	private List<Vacante> getVacantes()
-	{
-		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-		List<Vacante> lista = new LinkedList<Vacante>();
-		try
-		{
-			//Creamos la oferta de trabajo 1.
-			Vacante vacante1 = new Vacante();
-			vacante1.setId(1);
-			vacante1.setNombre("Ingeniero Civil");
-			vacante1.setDescripcion("Solicitamos ingeniero civil para diseñar puente peatonal");
-			vacante1.setFecha(sdf.parse("21-10-2022"));
-			vacante1.setSalario(8500.0);
-			vacante1.setDestacado(1);
-			vacante1.setImagen("empresa1.png");
-			
-			//Creamos la oferta de trabajo 2
-			Vacante vacante2 = new Vacante();
-			vacante2.setId(2);
-			vacante2.setNombre("Contador Publico");
-			vacante2.setDescripcion("Importante empresa solicita contador con 5 años de experiencia.");
-			vacante2.setFecha(sdf.parse("22-10-2022"));
-			vacante2.setSalario(12000.0);
-			vacante2.setDestacado(0);
-			vacante2.setImagen("empresa2.png");
-			
-			//Creamos la oferta de trabajo 3
-			Vacante vacante3 = new Vacante();
-			vacante3.setId(3);
-			vacante3.setNombre("Ingeniero Eléctrico");
-			vacante3.setDescripcion("Empresa internacional solicita Ingeniero mecánico para mantenimiento de instalación eléctrica. ");
-			vacante3.setFecha(sdf.parse("23-10-2022"));
-			vacante3.setSalario(10500.0);
-			vacante3.setDestacado(0);
-			
-			//Creamos la oferta de trabajo 4
-			Vacante vacante4 = new Vacante();
-			vacante4.setId(4);
-			vacante4.setNombre("Diseñador Gráfico");
-			vacante4.setDescripcion("Solicitamos Diseñador gráfico titulado para diseñar la publicidad de la empresa");
-			vacante4.setFecha(sdf.parse("24-10-2022"));
-			vacante4.setSalario(7500.0);
-			vacante4.setDestacado(1);
-			vacante4.setImagen("empresa3.png");
-			
-			/**
-			 * Agrefamos los 4 objetos de tipo vacante a la lista
-			 */
-			lista.add(vacante1);
-			lista.add(vacante2);
-			lista.add(vacante3);
-			lista.add(vacante4);
-		}
-		catch(ParseException e)
-		{
-			System.out.println("Error: " + e.getMessage());
-		}
-		return lista;
 	}
 }
---
+++ src/main/java/net/cmartinez/service/IVacantesService.java
@@ -0,0 +1,9 @@
+package net.cmartinez.service;
+
+import java.util.List;
+import net.cmartinez.model.Vacante;
+
+public interface IVacantesService 
+{
+	List<Vacante> buscarTodas();
+}
